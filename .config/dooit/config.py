8dc68b69b8da967b745179cf615cab5a93fa8a0d
AlphaSoundZ <jan.jacob.holst@gmail.com>
2024-03-24 12:52:20 +0100

    Dooit config (theme)

---
+++ .config/dooit/config.py
@@ -0,0 +1,48 @@
+black = "#101317"
+white = "#e5e9f0"
+grey = "#d8dee9"
+red = "#bf616a"
+frost_green = "#8fbcbb"
+cyan = "#88c0d0"
+green = "#a3be8c"
+yellow = "#ebcb8b"
+blue = "#81a1c1"
+magenta = "#b48ead"
+orange = "#d08770"
+
+base00 = "#101317"
+base01 = "#1a1d21"
+base02 = "#23262a"
+base03 = "#2b2e32"
+base04 = "#323539"
+base05 = "#c5c5c6"
+base06 = "#cbcbcc"
+base07 = "#d4d4d5"
+base08 = "#37d99e"
+base09 = "#f0a988"
+base0A = "#e5d487"
+base0B = "#e87979"
+base0C = "#37d99e"
+base0D = "#5fb0fc"
+base0E = "#c397d8"
+base0F = "#e87979"
+
+BACKGROUND = base00 # background color of the app
+BAR_BACKGROUND = base00 # background color of bar
+WORKSPACES_BACKGROUND = base00 # background color for workspaces pane
+TODOS_BACKGROUND = base00 # background color for todos pane
+BORDER_DIM = base04 # color for non-focused pane
+BORDER_LIT = base0F # color for focused pane
+
+SEARCH_COLOR = base0F
+
+WORKSPACE = {
+    "editing": base0F,
+    "start_expanded": True,
+}
+
+TODO = {
+    "editing": base0F,
+    "start_expanded": True,
+    "color_todos": True,
+}
